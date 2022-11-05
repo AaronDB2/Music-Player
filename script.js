6b79ba129d4d5b57a0543e206a78ff6578c33a22
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-05 17:06:47 +0100

    Made progress bar work

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -90,6 +92,17 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+	if (isPlaying) {
+		const { duration, currentTime } = e.srcElement;
+		// Update progress bar width
+		const progressPercent = (currentTime / duration ) * 100;
+		progress.style.width = `${progressPercent}%`;
+	}
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
